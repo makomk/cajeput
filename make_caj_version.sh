21a35f42eaef43321d547387a60187c6bcd30a76
Aidan Thornton <makosoft@googlemail.com>
2010-07-11 00:18:45 +0100

    Export correct version info in exported source archives.

--- make_caj_version.sh
+++ make_caj_version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set +e
 
-CAJ_GIT_REVISION=`git log HEAD^..HEAD|grep "^commit "|sed "s/commit //" `
-CAJ_GIT_DATE=`git log HEAD^..HEAD|grep "^Date: "|sed "s/Date: //"|sed 's/^ *//' `
+# For exported copies of the source code
+CAJ_GIT_REVISION='$Format:%H$'
+CAJ_GIT_DATE='$Format:%ad$'
+
+# Otherwise we get the live information from git itself.
+[[ "$CAJ_GIT_REVISION" == \$* ]] && CAJ_GIT_REVISION=`git log HEAD^..HEAD --pretty=format:%H  `
+[[ "$CAJ_GIT_DATE" == \$* ]] &&CAJ_GIT_DATE=`git log HEAD^..HEAD --pretty=format:%ad `
 
 cp caj_version.h.in caj_version.h.new
 sed -i "s/%CAJ_GIT_REVISION%/$CAJ_GIT_REVISION/g" caj_version.h.new
